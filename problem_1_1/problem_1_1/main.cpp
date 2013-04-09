6a9a179ad9f14eaa7d7733b6555f92ef1856c0bb
Vince <vince@cardinalblue.com>
2013-04-09 22:20:49 +0800

    init p1-1

---
+++ problem_1_1/problem_1_1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  problem_1_1
+//
+//  Created by Lee Haw on 13/4/9.
+//  Copyright (c) 2013年 Lee Haw. All rights reserved.
+//
+
+#include "systemc.h"
+
+int sc_main (int argc, char * argv[]) {
+			
+    // insert code here...
+    std::cout << "Hello, SystemC!\n";
+    sc_start();
+    return 0;
+}
+
